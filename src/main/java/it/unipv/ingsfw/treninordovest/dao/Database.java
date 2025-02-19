939477fe7ea1a5779e154e5023a6e662ba86abbb
andreaballare003 <ballareandrea09@gmail.com>
2025-02-19 16:10:43 +0100

    file di configurazione

---
+++ src/main/java/it/unipv/ingsfw/treninordovest/dao/Database.java
@@ -0,0 +1,15 @@
+package it.unipv.ingsfw.treninordovest.dao;
+
+public class Database {
+
+    private final String driver;
+    private final String url;
+    private final String user;
+    private final String pass;
+
+
+
+
+
+
+}
--- src/main/java/it/unipv/ingsfw/treninordovest/main/Main.java
+++ src/main/java/it/unipv/ingsfw/treninordovest/main/Main.java
@@ -8,33 +8,6 @@ public class Main {
     //Classe di prova -- Verrà rimossa in futuro
     public static void main(String[] args) {
 
-        try {
-
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/treninordovest","root","xxxx");
-
-            Statement stmt = con.createStatement();
-            //ResultSet res = stmt.executeQuery("SELECT ID,Nome,Cognome from utente");
-            ResultSet res = stmt.executeQuery("SELECT * from treno");
-
-            while (res.next()){
-                /*System.out.println(res.getString("ID"));
-                System.out.println(res.getString("Nome"));
-                System.out.println(res.getString("Cognome"));*/
-
-                System.out.println(res.getString("IDTreno"));
-                System.out.println(res.getString("Modello"));
-                System.out.println(res.getString("KW"));
-                System.out.println(res.getString("NumPosti"));
-
-            }
-
-        }catch (SQLDataException e){
-            e.printStackTrace();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
 
     }
-
 }
