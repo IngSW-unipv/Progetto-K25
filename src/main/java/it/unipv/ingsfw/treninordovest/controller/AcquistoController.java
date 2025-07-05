1842318e0e2473988331db9032bbe8e4231887c2
andreaballare003 <ballareandrea09@gmail.com>
2025-07-05 16:59:37 +0200

    Changes by Gabriel

--- src/main/java/it/unipv/ingsfw/treninordovest/controller/AcquistoController.java
+++ src/main/java/it/unipv/ingsfw/treninordovest/controller/AcquistoController.java
@@ -23,8 +23,9 @@ public class AcquistoController implements ActionListener {
     private final JTreniNordOvestFrame frame;
     private final AcquistoFacade acquistoFacade;
 
-   public AcquistoController(JTreniNordOvestFrame frame) {
-        this.view=frame.getCustomerMainPanel();
+    public AcquistoController(JTreniNordOvestFrame frame) {
+        this.view = frame.getCustomerMainPanel();
+        this.frame = frame;
         this.acquistoFacade = new AcquistoFacade();
 
         //Aggiunta dei listener
@@ -35,18 +36,17 @@ public class AcquistoController implements ActionListener {
     ///  Acquisto dei biglietti
     public void acquistoBiglietto() {
 
-        String idTratta= view.getTicketPurchasePanel().getTextFieldTratta().getText();
-        boolean ritorno= view.getTicketPurchasePanel().getCheckBoxRitorno().isSelected();
-        LocalDate dataRitorno= view.getTicketPurchasePanel().getDataRitorno().getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        String tipoBiglietto =view.getTicketPurchasePanel().getComboTipoBiglietto().getSelectedItem().toString();
+        String idTratta = view.getTicketPurchasePanel().getTextFieldTratta().getText();
+        boolean ritorno = view.getTicketPurchasePanel().getCheckBoxRitorno().isSelected();
+        LocalDate dataRitorno = view.getTicketPurchasePanel().getDataRitorno().getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        String tipoBiglietto = view.getTicketPurchasePanel().getComboTipoBiglietto().getSelectedItem().toString();
         String tipoPagamento = "cartacredito"; //Provvisorio
-        int quantita =  Integer.parseInt(view.getTicketPurchasePanel().getQuantitaSpinner().getValue().toString() ) ;
+        int quantita = Integer.parseInt(view.getTicketPurchasePanel().getQuantitaSpinner().getValue().toString());
 
-        if(acquistoFacade.acquistaBiglietto(tipoBiglietto,tipoPagamento,quantita,idTratta,ritorno,dataRitorno)) {
+        if (acquistoFacade.acquistaBiglietto(tipoBiglietto, tipoPagamento, quantita, idTratta, ritorno, dataRitorno)) {
             JOptionPane.showMessageDialog(view, "Biglietti acquistati");
         } else
-            JOptionPane.showMessageDialog(view, "Errore durante l'acquisto dei biglietti!","Errore",JOptionPane.ERROR_MESSAGE);
-
+            JOptionPane.showMessageDialog(view, "Errore durante l'acquisto dei biglietti!", "Errore", JOptionPane.ERROR_MESSAGE);
 
 
     }
@@ -54,15 +54,14 @@ public class AcquistoController implements ActionListener {
     ///  Acquisto dell'abbonamento
     public void acquistoAbbonamento() {
 
-       String tipoAbbonamento=view.getSubscriptionPanel().getComboTipo().getSelectedItem().toString();
-       String tipoAcquisto=null; //DA DEFINIRE
-       int quantita=1;
+        String tipoAbbonamento = view.getSubscriptionPanel().getComboTipo().getSelectedItem().toString();
+        String tipoAcquisto = null; //DA DEFINIRE
+        int quantita = 1;
 
-        if(acquistoFacade.acquistoAbbonamento(tipoAbbonamento,tipoAcquisto,quantita)){
+        if (acquistoFacade.acquistoAbbonamento(tipoAbbonamento, tipoAcquisto, quantita)) {
             JOptionPane.showMessageDialog(view, "Acquisto con successo!");
-        }
-        else
-            JOptionPane.showMessageDialog(view, "Abbonamento già posseduto o tessera non valida!","Errore",JOptionPane.ERROR_MESSAGE);
+        } else
+            JOptionPane.showMessageDialog(view, "Abbonamento già posseduto o tessera non valida!", "Errore", JOptionPane.ERROR_MESSAGE);
 
     }
 
@@ -72,9 +71,7 @@ public class AcquistoController implements ActionListener {
         if (acquistoFacade.acquistaTessera()) {
             JOptionPane.showMessageDialog(view, "Acquisto con successo!");
         } else
-            JOptionPane.showMessageDialog(view, "Errore !!! Tessera posseduta o non valida","Errore",JOptionPane.ERROR_MESSAGE);
-
-
+            JOptionPane.showMessageDialog(view, "Errore !!! Tessera posseduta o non valida", "Errore", JOptionPane.ERROR_MESSAGE);
 
 
     }
@@ -111,5 +108,6 @@ public class AcquistoController implements ActionListener {
                 break;
 
 
+        }
     }
 }
